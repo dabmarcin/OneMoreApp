a46ced2ab9ec6990ba2b80416d53deff1475958d
Marcin Dabrowski <dabmarcin@gmail.com>
2024-02-04 15:40:21 +0100

    if cases

---
+++ OneMoreApp/OneMoreApp/OneMoreApp/Program.cs
@@ -0,0 +1,16 @@
+﻿string name = "Ewa";
+char sex = 'f';
+int age = 25;
+
+if (sex == 'f' && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex != 'f' && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
